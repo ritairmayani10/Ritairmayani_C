0144f565c37416e2d038f72a582fa57a526a5378
ritairmayani10 <113254246+ritairmayani10@users.noreply.github.com>
2022-09-24 21:57:31 -0700

    Add files via upload

---
+++ Ritairmayani_2217051047_C...cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	int hargaBelanja, hargaBaju;
+	cin>>hargaBelanja;
+	
+	if (hargaBelanja < 50000)
+		cout<<"duit lu kurang";
+		
+	cin>>hargaBaju;
+	if (hargaBaju > 10000)
+		cout<<"dede pinter";
+		
+	else if (hargaBelanja, hargaBaju == 100000)
+		cout<<"dede ganteng";
+	
+	return 0;
+}
